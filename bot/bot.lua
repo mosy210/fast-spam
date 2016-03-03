a75c6277e8f3a01ce56ff6092e2cc0b64e258981
mosy210 <mosy210@users.noreply.github.com>
2016-03-03 06:32:43 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {137264699},
+    sudo_users = {176486521},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
